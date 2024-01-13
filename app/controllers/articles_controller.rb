4347427b820a5450a7f2ffa9749825238a6861f0
Hasan Rauf <HasanR@winmo.com>
2024-01-12 18:33:12 -0600

    Complete REST API for managing news articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,35 @@
 class ArticlesController < ApplicationController
+  # GET /articles
+  def index
+    @articles = Article.order(published_at: :desc)
+    render json: @articles.as_json(only: [:id, :title, :content, :author, :category, :published_at])
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      render json: @article.as_json(only: [:id, :title, :content, :author, :category, :published_at]), status: :created
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  # GET /articles/:id
+  def show
+    @article = Article.find(params[:id])
+    render json: @article.as_json(only: [:id, :title, :content, :author, :category, :published_at])
+  rescue ActiveRecord::RecordNotFound
+    render status: :not_found
+  end 
+
+  def method_not_allowed
+    render status: :method_not_allowed
+  end
+
+  private
+
+  def article_params
+    params.permit(:title, :content, :author, :category, :published_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles, only: [:index, :create, :show] do
+    member do
+      put '/', to: 'articles#method_not_allowed'
+      patch '/', to: 'articles#method_not_allowed'
+      delete '/', to: 'articles#method_not_allowed'
+    end
+  end
 end
